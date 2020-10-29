f1506c1102e9ea496810bc3f49aafee00ce74e35
osagiestar <okoedooc@yahoo.com>
2020-10-29 15:44:56 +0000

    working on Level 3

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-
+const bodyParser = require("body-parser")
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const welcomeMessage = {
   id: 0,
@@ -16,8 +19,87 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
+// Base route path // 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+// Gets all the messages as a JSON array //
+app.get("/messages/", (request, response) => {
+  response.json(messages);
+});
+
+// Obtains the chat message based on the Id //
+app.get("/messages/:id", (request, response) => {
+  const Id = request.params.id; // <- {Id:value}
+
+  const Message = messages.find((e) => e.id == Id);
+
+  if (Message) {
+    response.json(Message);
+  } else {
+    response.send("Message not found");
+  }
+});
+
+// Allows the assignment of a new message Id by increment of 1 //
+let newId = 1;
+app.post("/messages/", (req, res) => {
+  const message = {};
+  message.id = newId;
+  message.from = req.body.from;
+  message.text = req.body.text;
+
+  // destructuring the above code //
+  //  const message = {} ;
+  //  message.id = newId;
+  // const { from, text } = req.body;
+  // message.from = from;
+  // message.text = text;
+  // messages.push(message);
+
+  // Gives a status code and message for empty or missing text from property //
+  if (message.from != "" && message.text != "") {
+    messages.push(message);
+    newId++;
+    res.send("Messages added successfully");
+  } else {
+    // res.status(400).send("Message is not valid!");
+    res.sendStatus(400); // alternative using standard code message //
+  }
+  console.log(message);
+});
+
+// A message can be deleted based on the defined Id //
+app.delete("/messages/:Id", (req, res) => {
+  const { Id } = req.params;
+  messages.forEach((e) => {
+    if (e.id == Id) {
+      messages.splice(e, 1);
+    }
+  });
+  res.send("Message deleted");
+
+});
+
+// Search for messages based on the text input //
+app.get("/messages/search", (request, response) => {
+  const search = request.query.text; 
+response.json(messages.filter((message) => 
+message.text.toLowerCase().contains(search.toLowerCase())
+))
+});
+
+app.get("/messages/latest", (request, response) => {
+  const readMsg = request.query.latest; 
+  msgArr = [];
+  msgArr.push(messages.slice(Math.max(arr.length - 10, 1)))
+  response.json(msgArr)
+
+}).
+
+
+// Start Listening on : 3000 //
+app.listen(3000, function () {
+  console.log("Server is listening on port 3000. Ready to accept requests!");
+});
